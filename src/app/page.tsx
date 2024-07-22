dfefcd83c9a47cc6cfe6b80cd8a02c7bc016266b
Anshumali Karna <anshumali.karna99@gmail.com>
2024-07-22 21:24:33 +0530

    Hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <p className="text-3xl font-bold text-indigo-500">Hello Discord Clone</p>
+    <div className="">
+      <p className="text-3xl font-bold text-indigo-500">Hello OpenCord Clone</p>
+      <Button>Click Me</Button>
+    </div>
   );
 }
