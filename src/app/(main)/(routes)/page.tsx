29db17d2fc24bd1d06654ee856bba9989242741c
Anshumali Karna <anshumali.karna99@gmail.com>
2024-07-22 22:41:00 +0530

    Auth

--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="">
-      <p className="text-3xl font-bold text-indigo-500">Hello OpenCord Clone</p>
-      <Button>Click Me</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
