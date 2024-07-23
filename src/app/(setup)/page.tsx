61796fb0dcc0e0f1bc6697dae59bb9a1975b20b3
Anshumali Karna <anshumali.karna99@gmail.com>
2024-07-23 21:51:26 +0530

    profile find

--- src/app/(setup)/page.tsx
+++ src/app/(setup)/page.tsx
@@ -1,6 +1,25 @@
+import { redirect } from "next/navigation";
+
 import React from "react";
+import { initialProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/db";
+import { Profile } from "@prisma/client";
 
 const page = async () => {
+  const profile = await initialProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
   return <div>Create a Server </div>;
 };
 
