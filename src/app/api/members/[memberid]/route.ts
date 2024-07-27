4d871e339bc0b99659b078ba9e599e11fd6ac4c0
Anshumali Karna <anshumali.karna99@gmail.com>
2024-07-28 00:40:35 +0530

    Header 1

--- src/app/api/members/[memberid]/route.ts
+++ src/app/api/members/[memberid]/route.ts
@@ -1,4 +1,5 @@
 import { currentProfile } from "@/lib/currentProfile";
+import { db } from "@/lib/db";
 
 export async function PATCH(
   Req: Request,
@@ -6,6 +7,10 @@ export async function PATCH(
 ) {
   try {
     const profile = await currentProfile();
+    const { searchParams } = new URL(Req.url);
+    const { role } = await Req.json();
+
+    const serverId = searchParams.get("serverId");
 
     if (!profile) {
       return Response.json(
@@ -17,7 +22,87 @@ export async function PATCH(
         }
       );
     }
-    
+
+    if (!serverId) {
+      return Response.json(
+        {
+          message: "Serverid missing",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    if (!params.memberid) {
+      return Response.json(
+        {
+          message: "Memberid missing",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const server = await db.server.findFirst({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+    });
+
+    if (!server) {
+      return Response.json(
+        {
+          message: "Server not found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    const updateMember = await db.server.update({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+      data: {
+        members: {
+          update: {
+            where: {
+              id: params.memberid,
+              profileId: {
+                not: profile.id,
+              },
+            },
+            data: {
+              role,
+            },
+          },
+        },
+      },
+      include: {
+        members: {
+          include: {
+            profile: true,
+          },
+          orderBy: {
+            role: "asc",
+          },
+        },
+      },
+    });
+
+    return Response.json(
+      {
+        ...updateMember,
+      },
+      {
+        status: 200,
+      }
+    );
   } catch (error) {
     console.log(error);
     return Response.json(
